c182b1d6280bfbad7725be94579c5450d90ba59d
tkuprinskas <tkuprinskas@users.noreply.github.com>
2020-11-25 00:01:02 +0200

    solved some Java challenges

--- src/CSVReader/CSVReader.java
+++ src/CSVReader/CSVReader.java
@@ -4,10 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+
+import static java.time.chrono.JapaneseEra.values;
 
 public class CSVReader {
     public static void main(String[] args) {
-        String path = "C:\\Users\\tadas\\Desktop\\JAVA\\src\\JavaIntermediate\\CSVReader\\nba_ht_wt.csv";
+        String path = "C:\\Users\\tadas\\Desktop\\JAVA\\src\\CSVReader\\nba_ht_wt.csv";
         String line = "";
 
         try {
@@ -18,6 +21,7 @@ public class CSVReader {
             while ((line = br.readLine()) != null) {
 //                System.out.println(line);
                 String[] values = line.split(",");
+
                 System.out.println("Player: " + values[0] + ", Age: " + values[4]); // values [2] prints columns
             }
 
---
+++ src/CSVReader/Read.md
@@ -0,0 +1,2 @@
+Created a CSV Reader project. Used a .csv file to print out
+NBA players names and ages to console.
