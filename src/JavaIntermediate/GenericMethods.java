394f89dbb44d95e30a2b267f47322b622ef001ec
tkuprinskas <tkuprinskas@users.noreply.github.com>
2020-11-30 23:09:07 +0200

    created tic tac toe according to the tutorial, made some guis for bank app

---
+++ src/JavaIntermediate/GenericMethods.java
@@ -0,0 +1,29 @@
+package JavaIntermediate;
+
+public class GenericMethods {
+
+    public static void main(String[] args) {
+        Integer[] iray = {1,2,3,4};
+        Character[] cray = {'b', 'u', 'c', 'y'};
+        printMe(iray);
+        printMe(cray);
+    }
+//    public static void  printMe(Integer[] i){
+//        for (Integer x : i)
+//            System.out.printf("%s", x);
+//        System.out.println();                         overloaded methods
+//    }
+//    public static void  printMe(Character[] i){
+//        for (Character x : i)
+//            System.out.printf("%s", x);
+//        System.out.println();
+//    }
+
+    //generic method
+    public static <T> void printMe(T[] x){
+        for(T b : x)
+            System.out.printf("%s ", b);
+        System.out.println();
+    }
+
+}
---
+++ src/TicTacToe/README.md
@@ -0,0 +1,1 @@
+TicTacToe made according to youtube tutorial
